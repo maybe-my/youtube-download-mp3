8296c49dd07410ae4dd68f0431d4a7766df0efab
load <load3585@protonmail.com>
2021-08-29 22:00:15 +0300

    commit

---
+++ pars.py
@@ -0,0 +1,20 @@
+import youtube_dl
+def run():
+    video_url = "https://www.youtube.com/watch?v=AVhHtd28QQw"
+    video_info = youtube_dl.YoutubeDL().extract_info(
+        url = video_url,download=False
+    )
+    filename = f"{video_info['title']}.mp3"
+    options={
+        'format':'bestaudio/best',
+        'keepvideo':False,
+        'outtmpl':filename,
+    }
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+
+    print("Скачивание... {}".format(filename))
+
+if __name__=='__main__':
+    run()
